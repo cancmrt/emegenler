8d4c7b1eb0a9119a88414d4539d4ab5753d59996
cancmrt <cancm91@gmail.com>
2020-01-25 17:18:49 +0300

    Solution putting in build.sh file

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,7 +2,7 @@
 echo "Executing MSBuild DLL begin command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"${ORGANIZATION}" /k:"${PROJECT_KEY}" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${LOGIN}
 echo "Running build..."
-dotnet build Guard.Emegenler/Guard.Emegenler.csproj
+dotnet build EmegenlerSolution.sln
 echo "Running tests..."
 dotnet test EmegenlerTests/EmegenlerTests.csproj --logger:trx
 echo "Executing MSBuild DLL end command..."
