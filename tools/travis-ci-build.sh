55ea1f0129a3396c4e1dc71f6cd049e7e162e928
cancmrt <cancm91@gmail.com>
2020-01-26 19:41:30 +0300

    Travis.yml changes

--- tools/travis-ci-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Executing MSBuild DLL begin command..."
-dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"${ORGANIZATION}" /k:"${PROJECT_KEY}" /d:sonar.cs.vstest.reportsPaths="**/EmegenlerTests/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${LOGIN} /d:sonar.language="cs"
-echo "Running build..."
-dotnet build EmegenlerSolution.sln
-echo "Running tests..."
-dotnet test EmegenlerTests/EmegenlerTests.csproj --logger:trx --collect "Code Coverage"
-echo "Executing MSBuild DLL end command..."
-dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${LOGIN}
